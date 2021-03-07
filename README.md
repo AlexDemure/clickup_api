745c81c4ea817acd96fc1297c661076db18698b7
AlexDemure <alexanderdemure@gmail.com>
2021-03-07 18:37:48 +0500

    Инструкция ClickUp

--- README.md
+++ README.md
@@ -1,2 +1,88 @@
 # clickup_api
-Интеграция с ClickUp Service с применением API V2
+#### Инструкция по интеграции с ClickUp Service с применением API V2
+
+##### Процессы 
+- OAuth авторизация
+- ClickUp API V2 (Пользователи, команды, задачи.)
+
+##### Python зависимости
+- httpx
+- pydantic
+
+<br>
+
+##### Основной ресурс для работы  https://clickup.com/api
+## Добавления приложения в ClickUp
+- Создайте приложения в ClickUp <br>
+**WorkSpace >> Settings >> Integrations >> ClickUp API >> + Create an App** <br>
+ 
+Важно **redirect_uri** должны быть домены https если разрабатываете локально используйте **ngrok**
+Карточка приложения после создания:
+```
+App Name My App
+CLIENT ID 1PZ9zOi5sZNE6-93gHE5ZAQpX*******
+CLIENT SECRET RSwPUs8i2vsrxodnI-xEycxFbN-******
+REDIRECT URLS https://41095d8af8a2.ngrok.io/webhook/login
+```
+Если используете ngrok вам придется менять карточку приложения каждый раз когда истекает действие работы ngrok (каждые 2 часа)
+
+- Скопируйте и вставьте значения CLIENT_ID и CLIENT_SECRET в ваше приложение.
+
+## OAuth авторизация
+#### OAuth авторизация происходит следующим образом:
+![GitHub Logo](oauth.png)
+### Воспроизведение процесса кодом
+- Генерирование ссылки на получение Code
+```
+from clickup.service import ClickUpOAuth
+
+# State - Данные которые вернутся на вебхук
+# используется для идентификации пользователя
+state = dict(user_id=1)
+
+# Наш webhook который установили в ClickUpApp
+redirect_uri = https://41095d8af8a2.ngrok.io/webhook/login
+
+url = ClickUpOAuth.get_code_url(redirect_uri, state)
+print(url)
+
+>>> 'https://app.clickup.com/api?client_id=1PZ9zOi5sZNE6-93gHE5ZAQpX*******&state=eyJ4IjogMX0=&redirect_uri=https://41095d8af8a2.ngrok.io/webhook/login'
+```
+Эту ссылку необходимо отдать пользователю для перехода по ней. <br>
+Для допольнительной информации по get параметрам https://clickup.com/api >> Authentication <br>
+
+- Прием уведомления об успешном входе (WebHook)
+```
+from oauth.webhooks import login_success
+
+def login_success_to_system(request):
+   code, state = login_success(request)
+   print(code, state)
+
+>>> login_success_to_system(request)
+>>> 6655746, dict(user_id=1)
+```
+
+- Получение авторизационных данных (access_token)
+```
+from clickup.service import ClickUpOAuth
+
+token_data = await ClickUpOAuth().get_token(code, redirect_uri)
+
+print(token_data)
+>>> dict(token=dict(access_token=***))
+```
+После успешного получения токена авторизации можно использовать ClickUp API
+
+
+### Использование ClickUp API V2
+После получения токена авторизации его необходимо передавать в Headers каждого запроса.
+```
+from clickup.service import ClickUp
+
+clickup = ClickUp(f"{token_data['token']['access_token']}")
+
+click_up_user = await clickup.get_user()
+print(click_up_user)
+>>> dict(user=dict(id=123, name=AlexDemure, email=your_email@gmail.com)
+```
---
+++ clickup/service.py
@@ -0,0 +1,81 @@
+
+from common.base_class import APIClass
+from oauth.service import OAuth, OAuthUtils
+from .schemas import ClickUpTasks
+from .serializer import prepare_task
+from .settings import click_up_settings
+
+
+class Users(APIClass):
+
+    async def get_user(self) -> dict:
+        """Получение пользовательских данных"""
+        url = 'https://api.clickup.com/api/v2/user'  # Получение данных о пользователе по токену.
+
+        r_json = await self.make_request("GET", url)
+        return r_json['user']
+
+
+class Teams(APIClass):
+
+    async def get_teams(self) -> list:
+        url = 'https://api.clickup.com/api/v2/team'  # Получение данных о пользователе по токену.
+
+        r_json = await self.make_request("GET", url)
+        return r_json['teams']
+
+
+class Tasks(APIClass):
+
+    async def get_tasks(self, team_id: int, click_user_id: int):
+        url = f"https://api.clickup.com/api/v2/team/{team_id}/task?assignees%5B%5D={click_user_id}"
+
+        r_json = await self.make_request("GET", url)
+        return r_json['tasks']
+
+
+class ClickUp(Users, Teams, Tasks):
+    """
+    Класс для работы с ClickUP API.
+    """
+
+    async def collect_user_tasks(self, click_user_id: int) -> ClickUpTasks:
+        """
+        Метод по сбору назначенных задач на пользователя по всему WorkSpace.
+
+        Данный метод определяет тот WorkSpace который был выбран при подключении ClickUp и собирает по нему данные.
+        Данные собираются по всем областям перебирая каждую сущность: Spaces, Folders, Folders List, Tasks.
+        Перебор происходит из-за архитектуры clickup которая деклалирует сбор данных по этапно.
+        Чтобы добраться до Tasks необходимо произвести всю цепочку.
+        WorkSpace -> Spaces (list) -> Folders (list) -> FoldersLists (lists) -> Tasks (lists)
+        """
+        teams = await self.get_teams()
+
+        prepared_tasks = []
+        for team in teams:
+            tasks = await self.get_tasks(team['id'], click_user_id)
+            prepared_tasks += [prepare_task(x) for x in tasks if x['folder']['name'] != "Backlog"]
+
+        return ClickUpTasks(tasks=prepared_tasks)
+
+
+class ClickUpOAuth(OAuth):
+    """Класс с полным процессом получения авторизационного токена для работы с ClickUp."""
+
+    client_id = click_up_settings.CLICKUP_CLIENT_ID
+    client_secret = click_up_settings.CLICKUP_SECRET_KEY
+
+    @staticmethod
+    def get_code_url(redirect_uri: str, state: dict) -> str:
+        """Шаг №1 Получение ссылки с кодом подтверждения."""
+        return f"https://app.clickup.com/api" \
+               f"?client_id={click_up_settings.CLICKUP_CLIENT_ID}" \
+               f"&state={OAuthUtils.encode_state_to_base64(state)}" \
+               f"&redirect_uri={redirect_uri}"
+
+    @classmethod
+    def get_token_url(cls, code: str, redirect_uri: str = None):
+        return f'https://api.clickup.com/api/v2/oauth/token?' \
+              f'client_id={click_up_settings.CLICKUP_CLIENT_ID}&' \
+              f'client_secret={click_up_settings.CLICKUP_SECRET_KEY}&' \
+              f'code={code}'
